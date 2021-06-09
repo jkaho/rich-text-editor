220c22f0c936572339757a223c1febf347a29c77
jkaho <72959522+jkaho@users.noreply.github.com>
2021-06-09 22:48:43 +1000

    Add toggle styles, make editor full-screen

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ function init() {
   textbox = document.getElementById("textbox");
 }
 
-function formatText(command, value) {
+function formatText(event, command, value) {
+  if (event.target.value) {
+    if (event.target.value === "off") {
+      event.target.value = "on";
+      event.target.classList.add("format-on");
+    } else {
+      event.target.value = "off";
+      event.target.classList.remove("format-on");
+    }
+  }
+
   textbox.focus();
   document.execCommand(command, false, value);
   textbox.focus();
 }
 
-document.getElementById("bold").addEventListener("click", () => formatText("bold"));
-document.getElementById("italic").addEventListener("click", () => formatText("italic"));
-document.getElementById("underline").addEventListener("click", () => formatText("underline"));
-document.getElementById("insertUnorderedList").addEventListener("click", () => formatText("insertUnorderedList"));
-document.getElementById("insertOrderedList").addEventListener("click", () => formatText("insertOrderedList"));
+document.getElementById("bold").addEventListener("click", (event) => formatText(event, "bold"));
+document.getElementById("italic").addEventListener("click", (event) => formatText(event, "italic"));
+document.getElementById("underline").addEventListener("click", (event) => formatText(event, "underline"));
+document.getElementById("insertUnorderedList").addEventListener("click", (event) => formatText(event, "insertUnorderedList"));
+document.getElementById("insertOrderedList").addEventListener("click", (event) => formatText(event, "insertOrderedList"));
