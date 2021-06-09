27545436e8891bacd5f90a49a82ca799b8dcde85
jkaho <72959522+jkaho@users.noreply.github.com>
2021-06-09 23:34:49 +1000

    Add font select, modify button toggle logic

--- assets/script.js
+++ assets/script.js
@@ -4,22 +4,33 @@ function init() {
   textbox = document.getElementById("textbox");
 }
 
-function formatText(event, command, value) {
-  if (event.target.value) {
-    if (event.target.value === "off") {
-      event.target.value = "on";
-      event.target.classList.add("format-on");
-    } else {
-      event.target.value = "off";
+function formatText(event, command, commandValue) {
+  if (event.target.matches("button") && event.target.classList.contains("toggle")) {
+    if (document.queryCommandState(event.target.id)) {
       event.target.classList.remove("format-on");
+    } else {
+      event.target.classList.add("format-on");
     }
-  }
+  } 
 
   textbox.focus();
-  document.execCommand(command, false, value);
+  document.execCommand(command, false, commandValue);
   textbox.focus();
 }
 
+function formatButtons() {
+  const formats = ["bold", "italic", "underline"];
+  formats.forEach(format => {
+    if (document.queryCommandState(format)) {
+      document.getElementById(format).classList.add("format-on");
+    } else {
+      document.getElementById(format).classList.remove("format-on");
+    }
+  });
+}
+
+document.getElementById("textbox").addEventListener("input", formatButtons);
+document.getElementById("fontName").addEventListener("change", (event) => formatText(event, "fontName", event.target[event.target.selectedIndex].value));
 document.getElementById("bold").addEventListener("click", (event) => formatText(event, "bold"));
 document.getElementById("italic").addEventListener("click", (event) => formatText(event, "italic"));
 document.getElementById("underline").addEventListener("click", (event) => formatText(event, "underline"));
